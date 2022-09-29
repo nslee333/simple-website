dc79c48f41caf36615e96f43291bf967fdad5cef
Kyle McLeod <kcloud99@Dev-macbook.local>
2022-09-29 16:15:33 -0700

    add 2 sponsor links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { StyledFooter, FlexDiv } from "../UI/Common";
+import { StyledFooter, FlexDiv, StyledImg } from "../UI/Common";
 
 export default function Footer(props) {
   return (
     <StyledFooter>
-      <FlexDiv justify="flex-start" align="center">
-        Our little JavaScript community in Bend, Oregon.
+      <FlexDiv justify="flex-start" align="flex-start" direction="column">
+        <FlexDiv justify="flex-start" align="center">
+          Our little JavaScript community in Bend, Oregon. Thank you so much to our excellent sponsors!
+        </FlexDiv>
+        <FlexDiv justify="flex-start" align="center">
+          <a href="https://www.codepen.io" target="_blank">
+            <StyledImg src="/images/codepen_logo.png" alt="Codepen" />
+          </a>
+          <a href="https://www.uptechstudio.com" target="_blank">
+            <StyledImg src="/images/uptech_logo.png" alt="Uptech Studio" />
+          </a>
+        </FlexDiv>
       </FlexDiv>
     </StyledFooter>
   );
