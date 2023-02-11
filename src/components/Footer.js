73f1b43d66f7f02dd0a35df169f0c62e1309bfae
Nathan <nslee333@gmail.com>
2023-02-10 17:38:05 -0800

    Updated footer and header.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { StyledFooter, FlexDiv, StyledImg } from "../UI/Common";
+import { StyledFooter, FlexDiv } from "../UI/Common";
 
-export default function Footer(props) {
+export default function Footer() {
   return (
     <StyledFooter>
       <FlexDiv justify="flex-start" align="flex-start" direction="column">
         <FlexDiv justify="flex-start" align="center">
-          Our little JavaScript community in Bend, Oregon. Thank you so much to our excellent sponsors!
+
         </FlexDiv>
         <FlexDiv justify="flex-start" align="center">
-          <a href="https://www.codepen.io" target="_blank" rel="noopener noreferrer">
-            <StyledImg src="/images/codepen_logo.png" alt="Codepen" />
-          </a>
-          <a href="https://www.uptechstudio.com" target="_blank" rel="noopener noreferrer">
-            <StyledImg src="/images/uptech_logo.png" alt="Uptech Studio" />
-          </a>
+          
         </FlexDiv>
       </FlexDiv>
     </StyledFooter>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { StyledHeader } from '../UI/Common';
+import React from "react";
+import { StyledHeader } from "../UI/Common";
+import bendjs_logo from "../images/bendjs_logo_square.png"
 
-export default function Header({ children }) {
+export default function Header() {
 
   return (
     <StyledHeader>
-      {children}
+      <div>
+        <img className="BendJSLogo"src={bendjs_logo} alt="bendjs logo"></img>
+      </div>
+      <div className="SocialsLink">
+        <a url="" alt="bendjs logo">/BendJS</a>
+      </div>
     </StyledHeader>
   )
 }
