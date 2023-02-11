8cc1ec3465f688e283738410a35198cb8ff5256d
Nathan <nslee333@gmail.com>
2023-02-10 17:38:28 -0800

    WIP: Styling for Welcome.js.

--- src/UI/Common.js
+++ src/UI/Common.js
@@ -1,49 +1,107 @@
 import styled from "styled-components";
 
 const StyledModule = styled.div`
-  background: white;
-  padding: 2rem;
-  border-radius: 5px;
-  box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.3);
-  min-height: 250px;
-  display: flex;
-  flex-direction: column;
-  .heading {
-    font-size: 1.17em;
-    color: red;
-    text-transform: uppercase;
-  }
-  .subheading {
-    margin-bottom: 1rem;
-    font-size: 1.5em;
-    font-weight: bold;
-  }
-  .button {
-    margin-top: auto;
-  }
+  // background: white;
+  // padding: 2rem;
+  // border-radius: 5px;
+  // box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.3);
+  // min-height: 250px;
+  // display: flex;
+  // flex-direction: column;
+  // .heading {
+  //   font-size: 1.17em;
+  //   color: red;
+  //   text-transform: uppercase;
+  // }
+  // .subheading {
+  //   margin-bottom: 1rem;
+  //   font-size: 1.5em;
+  //   font-weight: bold;
+  // }
+  // .button {
+  //   margin-top: auto;
+  // }
 `;
 
 const StyledHeader = styled.header`
-  border-bottom: 10px solid black;
-  border-right: 10px solid black;
-  padding: 3rem 1.2rem 1rem 0;
-  text-align: right;
-  margin-right: calc(50% - 700px / 2);
-  margin-bottom: 2rem;
-  background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/mountains.svg);
-  background-repeat: no-repeat;
-  background-position: bottom 10px right 110px;
-  h1 {
-    font-size: 3rem;
+
+border: 2px solid white;
+display: flex;
+justify-content: space-around;
+
+height: min(30vw, 12rem);
+// width: min(rem, 1000vw);
+width: 133.5rem;
+padding: 1rem;
+
+
+
+.BendJSLogo {
+  border-radius: min(1rem, 2vw);
+  width: min(9rem, 25vw);
+  height: auto;
+  
+  
+  margin-right: min(1rem, 50vw);
+  margin-top: min(1rem, 2vw);
+
+  
+
+  // @media (max-width: 700px) {
+  //   // margin-left: 8rem;
+  //   // margin-top: 2rem;
+  // }
+}
+
+.SocialsLink {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: min(17rem, 100vw);
+  height: min(4rem, 17vw);
+  font-size: min(3rem, 10vw);
+  
+  margin-right: min(1rem, 50vw);
+  margin-top: min(4rem, 6vw);
+
+}
+`;
+
+const StyledWelcome = styled.div`
+
+  margin-top: min(10rem, 10vw);
+
+
+
+  .StyledWelcome__Div {
+    border: 2px solid white;
+    display: columns;
+    align-items: center;
+    width: min(70rem, 70vw);
+    width: 80rem;
   }
-  @media (max-width: 700px) {
-    margin-right: 1rem;
+  
+  height: auto;
+
+
+  font-size: min(3rem, 3vw);
+
+  padding: min(1rem, 1vw);
+  border: 2px solid white;
+
+  h4, h5 {
+    margin: max(1rem, 1vw);
   }
+
+  h4 {
+    font-style: italic;
+    font-weight: bold;
+  }
+
 `;
 
 const StyledFooter = styled.footer`
-  border-top: 10px solid black;
-  border-left: 10px solid black;
   padding: 1rem 0 3rem 1rem;
   margin-left: calc(50% - 700px / 2);
   margin-top: 2rem;
@@ -61,7 +119,7 @@ const FlexDiv = styled.div`
   justify-content: ${({ justify }) => justify || "center"};
   width: ${({ width }) => width || "100%"};
   height: ${({ height }) => (height ? height : "100%")};
-  margin: 5px;
+  // margin: 5px;
 `;
 
 const StyledImg = styled.img`
@@ -70,5 +128,5 @@ const StyledImg = styled.img`
   margin-left: 10px;
 `;
 
-export { StyledModule, StyledHeader, StyledFooter, FlexDiv, StyledImg };
+export { StyledModule, StyledHeader, StyledWelcome, StyledFooter, FlexDiv, StyledImg };
 
