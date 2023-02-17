5397d5e7ca676389b5a90a5be3822f75885b5425
Nathan <nslee333@gmail.com>
2023-02-17 12:54:00 -0800

    Updated Footer.js component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,46 @@
 import React from "react";
 import styled from "styled-components";
+import GitHubLogo from "../images/github-mark.svg";
 
 const StyledFooter = styled.footer`
-  padding: 1rem 0 3rem 1rem;
-  margin-left: calc(50% - 700px / 2);
-  margin-top: 2rem;
-  min-height: 50px;
+  margin-top: 5rem;
+  margin-bottom: 5rem;
+  display: flex;
+  justify-content: center;
+  font-size: 1.25rem;
+  // border: 2px solid hotpink;
+
+  div {
+    display: grid;
+    justify-content: center;
+    padding: .5rem;
+  }
+
+  img {
+    margin-left: 5rem;
+    margin-top: .5rem;
+  }
+
+  @media (max-width: 886px) {
+    div {
+      transform: scale(0.85);
+    }
+  }
 `;
 
 
 
 export default function Footer() {
   return (
-    <StyledFooter>
-      
-    </StyledFooter>
+    <>
+      <StyledFooter>
+        <a href='https://github.com/BendJS/simple-website'>
+          <div>
+            Contribute to this website
+            <img src={GitHubLogo} alt='GitHub Logo'/>
+          </div>
+        </a>
+      </StyledFooter>
+    </>
   );
 }
