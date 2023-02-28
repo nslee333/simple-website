592702d0c4f811a3eedb0b3d9e994161ee1e2390
Nathan <nslee333@gmail.com>
2023-02-27 17:18:07 -0800

    Converted App.js to index.js, Added default html files to _document.js deleted old files.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,26 @@
-import Head from 'next/head';
-import styles from '';
+import Home from "./screens/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
+import "./App.scss";
 
-export default function Home() {
+function App() {
   return (
     <>
-      <Head>
-      <meta charset="utf-8" />
-      <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#000000" />
-      <meta
-        name="description"
-        content="Web site created using create-react-app"
-      />
-      <meta charset="UTF-8" />
-      <link rel="apple-touch-icon" href="logo192.png" />
-      {/* <!--
-        manifest.json provides metadata used when your web app is installed on a
-        user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
-      --> */}
-      <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-      {/* <!--
-        Notice the use of %PUBLIC_URL% in the tags above.
-        It will be replaced with the URL of the `public` folder during the build.
-        Only files inside the `public` folder can be referenced from the HTML.
+    
+      <div className="app">
+        <Header>
+          <h1>BendJS</h1>
+        </Header>
 
-        Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
-        work correctly both with client-side routing and a non-root public URL.
-        Learn how to configure a non-root public URL by running `npm run build`.
-      --> */}
-      <title>BendJS Website</title>
-      </Head>
-      <body>
-        <noscript>You need to enable JavaScript to run this app.</noscript>
-        <form name="newsletter" netlify netlify-honeypot="bot-field" hidden>
-          <input type="text" name="name" />
-          <input type="email" name="email" />
-        </form>
-        <div id="root"></div>
-        {/* <!--
-          This HTML file is a template.
-          If you open it directly in the browser, you will see an empty page.
+        <main>
+            <Home />
+        </main>
 
-          You can add webfonts, meta tags, or analytics to this file.
-          The build step will place the bundled scripts into the <body> tag.
-
-          To begin the development, run `npm start` or `yarn start`.
-          To create a production bundle, use `npm run build` or `yarn build`.
-        --> */}
-      </body>
+        <Footer />
+      </div>
     </>
   );
 }
+
+export default App;
