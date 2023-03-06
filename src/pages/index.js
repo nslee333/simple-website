040fb3869bd7e2cd9f9daa901add9e90d42a22b4
Nathan <nslee333@gmail.com>
2023-03-06 08:55:10 -0800

    Added image fetching from Sanity, passing props down to Gallery.js Added image to next config.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
-import Home from "../screens/Home";
+import Home from "./Home";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { createClient } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
 
-function App() {
+
+function App({images, member}) {
   return (
     <>
       <div className="app">
@@ -11,13 +14,45 @@ function App() {
         </Header>
 
         <main>
-            <Home />
+            <Home images={images} member={member}/>
         </main>
 
         <Footer />
       </div>
     </>
   );
+}
+
+const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: '2023-03-05',
+  useCdn: false
+});
+
+const builder = imageUrlBuilder(client)
+
+export function urlFor(source) {
+  return builder.image(source)
+}
+
+export async function getStaticProps() {
+  const images = await client.fetch(`*[_type == "ImageGallery"]{
+    image1, 
+    image2, 
+    image3, 
+    image4, 
+    image5, 
+    image6
+  }`);
+  const member = await client.fetch(`*[_type == 'MemberSpotlight']`);
+  
+  return {
+    props: {
+      images,
+      member
+    }
+  }
 }
 
 export default App;
