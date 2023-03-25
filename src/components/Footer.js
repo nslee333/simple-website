f172165ea40da54d627ce4d19032a5f655507d0c
Nathan <nslee333@gmail.com>
2023-03-25 16:12:27 -0700

    Footer to tailwind.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,54 +1,23 @@
 import React from "react";
-import styled from "styled-components";
 import github_logo from "../../public/images/github_logo_white.svg";
 import Image from "next/image";
 
-const StyledFooter = styled.footer`
-justify-content: center;
-display: flex;
-
-margin-top: 3.5rem;
-margin-bottom: 3.5rem;
-
-font-size: 1.25rem;
-
-div {
-    color: #fff;
-    justify-content: center;
-
-    display: grid;
-    padding: .5rem;
-  }
-
-  .Footer__github-logo {
-    height: 5rem;
-    width: 5rem;
-
-    margin-left: 5rem;
-    margin-top: .5rem;
-  }
-
-  @media (max-width: 886px) {
-    margin-top: 0rem;
-    margin-bottom: 1.5rem;
-    
-    div {
-      transform: scale(0.85);
-    }
-  }
-`;
 
 export default function Footer() {
   return (
     <>
-      <StyledFooter>
+      <div className='flex p-10 justify-center m-5'>
         <a href='https://github.com/BendJS/simple-website'>
-          <div>
-            Contribute to this website
-            <Image src={github_logo} alt='GitHub Logo'className='Footer__github-logo'/>
+          <div className='text-white'>
+            <div>
+              Contribute to this website
+            </div>
+            <div className='p-2 flex justify-center'>
+              <Image src={github_logo} alt='GitHub Logo' className=''/>
+            </div>
           </div>
         </a>
-      </StyledFooter>
+      </div>
     </>
   );
 }
