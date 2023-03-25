2b9d54ea68f5c62d9c2dc1dc951393d119ce4096
Nathan <nslee333@gmail.com>
2023-03-24 18:25:35 -0700

    Migrated Welcome from styled component to Tailwind.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,57 +1,21 @@
 import React from "react";
-import styled from "styled-components";
-
-const StyledWelcome = styled.div`
-  justify-content: center;
-  display: flex;
-
-  margin-top: min(13rem, 13vw);
-  height: auto;
-
-  font-size: max(2rem, 2vw);
-  color: #fff;
-  
-  div {
-    align-items: center;
-    display: columns;
-
-    width: min(70rem, 70vw);
-    min-width: 22rem; 
-  }
-  
-
-  h4, h5 {
-    margin: max(1rem, 1vw);
-  }
-
-  h4 {
-    font-style: italic;
-  }
-
-  @media (max-width: 400px) {
-    transform: scale(0.9);
-    margin-top: -4rem;
-    
-    margin-bottom: -2rem;
-  }
-`;
 
 export default function Welcome () {
   return (
     <>
-      <StyledWelcome>
-        <div>
-          <h4>BendJS</h4>
-          <h5>A Javascript meetup in Bend, Oregon</h5>
-          <h5>
+      <div className='flex justify-center pl-5 pt-10 pr-5'>
+        <div className='italic text-white w-[50rem] '>
+          <h4 className='text-4xl my-5'>BendJS</h4>
+          <h5 className='text-3xl my-5'>A Javascript meetup in Bend, Oregon</h5>
+          <h5 className='text-3xl my-10'>
             This group is for anyone interested in learning Javascript, 
             sharing something they know, networking with other devs, and having a great time! 
           </h5>
-          <h5>
+          <h5 className='text-3xl my-10'>
             All levels are welcome. Please join our slack workspace for future updates and continuing the conversation.
           </h5>
         </div>
-      </StyledWelcome>
+      </div>
     </>
   )
 }
