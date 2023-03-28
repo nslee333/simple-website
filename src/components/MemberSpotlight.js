93f0ef3be4bbe00556baad33281b86df869ddb36
Nathan <nslee333@gmail.com>
2023-03-28 09:48:54 -0700

    MemberSpotlight to tailwind.

--- src/components/MemberSpotlight.js
+++ src/components/MemberSpotlight.js
@@ -1,225 +1,129 @@
-import styled from "styled-components";
 import Image from "next/image";
-import {createClient} from 'next-sanity';
+import { createClient } from "next-sanity";
 import { urlFor } from "../pages";
-
-const StyledMemberSpotlight = styled.div`
-  margin-top: 5rem;
-  margin-bottom: 5rem;
-  
-  padding-bottom: 2rem;
-
-  background-color: #000;
-  color: #fff;
-  
-  .title-container {
-    justify-content: center;
-    display: flex;
-
-    padding: 2rem;
-    font-size: 30px;
-
-    font-weight: medium;
-  }
-
-  .member-container {
-    justify-content: space-evenly;
-    align-items: center;
-
-    flex-wrap: wrap;
-    display: flex;
-
-    margin-top: 5rem;
-    margin-bottom: 5rem;
-
-    height: auto;
-    background-color: #000;
-
-
-    @media (max-width: 1118px) {
-      margin-top: 2rem;
-      margin-botttom: 2rem;
-
-      padding-bottom: 2rem;
-      padding-top: 2rem;
-
-      .member-container__bio-teal-frame {
-        margin-top: 2rem;
-      }
-    }
-
-    @media (max-width: 520px) {
-      transform: scale(0.90);
-      margin-top: -2rem;
-
-      margin-bottom: 0;
-      padding-top: 1rem;
-
-      padding-bottom: 1rem;
-    }
-
-    @media (max-width: 420px) {
-      max-height: 55rem;
-      transform: scale(0.75);
-
-      margin-top: -5rem;
-      margin-bottom: 0;
-
-      .title-container {
-        margin-bottom: 0;
-        padding: .5rem;
-      }
-
-      .member-container__photo-teal-frame {
-        margin-top: 0;
-        margin-bottom: 0;
-
-        width: auto;
-        height: auto;
-      }
-      
-      .member-container__photo-black-frame {
-        min-height: 0;
-      }
-      
-      .member-container__photo {
-        margin-top: 2rem;
-        margin-left: 2rem;
-      }
-    }
-  }
-
-  .member-container__photo-teal-frame {
-    border-radius: 7px;
-    min-width: 24rem;
-
-    min-height: 24rem;
-    padding: 1rem;
-
-    background-color: #7FE7D7;
-
-    @media (max-width: 340px) {
-      margin-top: 0;
-      margin-bottom: 0;
-      transform: scale(0.9);
-    }
-  }
-  
-  .member-container__photo-black-frame {
-    display: flex;
-    border-radius: 7px;
-        
-    width: 24rem;
-    height: 24rem;
-    
-    background-color: #000;
-  }
-  
-  .member-container__photo {
-    margin-left: 2rem;
-    margin-top: 2rem;
-
-    border-radius: 7px;
-    width: 20rem;
-
-    height: 20rem;
-  }
-  
-  .member-container__bio-teal-frame {
-    border-radius: 7px;
-    padding: 1rem;
-
-    min-width: 24rem;
-    min-height: 24rem;
-    
-    background-color: #7FE7D7;
-
-    @media (max-width: 340px) {
-      transform: scale(0.9);
-    }
-  }
-    
-  .member-container__bio-black-frame {
-    border-radius: 7px;
-    padding: max(1rem, 1vw);
-
-    max-width: 40rem;
-    min-width: 22rem;
-    
-    max-height: 40rem;
-    min-height: 22rem;
-
-    background-color: #000;
-  }
-
-  .member-container__bio-member-name {
-    margin-bottom: max(2rem, 2vw);
-    font-size: 25px;
-  }
-  
-  .member-container__bio-text {
-    font-size: 20px  
-  }
-
-  @media (max-width: 560px) {
-    margin-top: 2rem;
-    margin-bottom: 2rem;
-
-    padding: 0;
-
-    .title-container {
-      padding-top: 5rem;
-    }
-  }
-
-  media (max-width: 520px) {
-    margin-bottom: 2rem;
-  }
-
-  @media (max-width: 420px) {
-    height: 67rem;
-        
-    padding-top: 0rem;
-    padding-bottom: 0rem;
-  }
-
-`;
 
 export default function MemberSpotlight(member) {
   return (
     <>
-      {member.props ? 
-        (<StyledMemberSpotlight>
-          <div className='title-container'>
-            <h5>Member Spotlight</h5>
-          </div>
-          <div className='member-container'>
-            <div className='member-container__photo-teal-frame'>
-              <div className='member-container__photo-black-frame'>
-                <Image src={urlFor(member.props[0].memberImage).url()} className='member-container__photo' alt='member profile' width={150} height={150}/>
+      {member.props ? (
+        <div className="sm:py-15  bg-black p-5 xs:my-0 xs:flex-wrap  md:block  ">
+          <h5
+            className=" mx-1
+                        flex
+                        justify-center 
+                        p-5 
+                        text-white 
+                        xs:text-2xl 
+                        sm:text-3xl"
+          >
+            Member Spotlight
+          </h5>
+
+          <div className="xs:mx-1 xs:flex xs:justify-center ">
+            <div className="align-center block justify-around  xs:p-0 md:flex md:w-[50rem] lg:w-[70rem]">
+              <div className="">
+                <div
+                  className=" rounded-md
+                            bg-[#7FE7D7]
+                              p-0.5 
+                              align-middle
+
+                              xs:my-2 
+                              xs:max-h-[12.5rem]
+                              xs:min-h-[9rem]
+                              xs:min-w-[5rem]
+
+                              xs:max-w-[12.5rem]
+                              sm:m-2
+
+                              md:m-5
+                              md:max-h-[15.3rem]
+                              md:min-h-[16rem]
+                              
+                              md:min-w-[16rem] 
+                              md:max-w-[16rem]
+                              min-[964px]:min-h-[13rem]
+                              min-[964px]:min-w-[14rem]
+
+                              lg:max-h-[30rem]
+                              lg:max-w-[30rem]"
+                >
+                  <div
+                    className=" m-5
+                            h-[22rem]
+                            max-h-[20rem]
+
+                            min-h-[18rem]
+                            w-[20rem]
+                            min-w-[14rem]
+                            max-w-[25rem] 
+                            rounded-md
+
+                            bg-black 
+                            xs:m-2
+
+                            xs:max-h-[11rem]
+                            xs:min-h-[11rem] 
+                            xs:min-w-[11rem]
+                            xs:max-w-[11rem]
+                            xs:p-5
+
+                            md:max-h-[14rem]
+                            md:min-h-[14rem] 
+                            md:min-w-[14rem]
+                            md:max-w-[14rem]
+
+                            lg:max-h-[20rem]
+                            lg:max-w-[20rem]
+                            "
+                  >
+                    <Image
+                      className="content-center rounded-md"
+                      src={urlFor(member.props[0].memberImage).url()}
+                      alt="member profile"
+                      width={300}
+                      height={175}
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className='member-container__bio-teal-frame'>
-              <div className='member-container__bio-black-frame'>
-                <h5 className='member-container__bio-member-name'>{member.props[0].memberName}</h5>
-                <div className='member-container__bio-text'>
-                {member.props[0].memberBio}
+              <div
+                className="h-auto
+                            min-h-[10rem] 
+                            min-w-[10rem] 
+                            max-w-[30rem]
+                            rounded-md
+                            bg-[#7FE7D7] 
+                            xs:m-0 
+                            xs:p-0.5
+                            sm:m-2
+                            md:m-5
+                            lg:max-w-[40rem]"
+              >
+                <div className="m-5 rounded-md bg-black  xs:m-2 xs:p-2">
+                  <h5 className="sm:text-md my-2  text-white sm:text-lg md:text-xl lg:text-2xl">
+                    {member.props[0].memberName}
+                  </h5>
+                  <div className=" md:text-md md:text-md text-white xs:text-sm lg:text-lg">
+                    {member.props[0].memberBio}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </StyledMemberSpotlight>)
-       : (<div></div>)}
+        </div>
+      ) : (
+        <div></div>
+      )}
     </>
   );
 }
 
-
 const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: '2023-03-05',
-  useCdn: false
+  apiVersion: "2023-03-05",
+  useCdn: false,
 });
 
 export async function getStaticProps() {
@@ -227,7 +131,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      member
-    }
-  }
+      member,
+    },
+  };
 }
