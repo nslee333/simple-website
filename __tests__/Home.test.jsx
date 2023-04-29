8b23a9869b44a499ffa6b01cf11ade5a91afaa1e
Nathan <nslee333@gmail.com>
2023-04-29 08:00:40 -0700

    Working Home smoke test.

--- __tests__/Home.test.jsx
+++ __tests__/Home.test.jsx
@@ -1,7 +1,7 @@
 import {render, screen} from "@testing-library/react";
+import Home from "../src/pages/Home"
 import "@testing-library/jest-dom"
 
 test("Renders Home without crashing", () => {
-  // render(<Home />);
-  // console.log("Unimple")
+  render(<Home />);
 });
