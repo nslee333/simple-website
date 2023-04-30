9e83f00c22b505cf13ee25da8944b0ba85b01820
Nathan <nslee333@gmail.com>
2023-04-29 18:59:16 -0700

    Unit tests for Socials.

--- __tests__/Socials.test.jsx
+++ __tests__/Socials.test.jsx
@@ -1,6 +1,34 @@
-import {render } from "@testing-library/react";
+import {render, screen } from "@testing-library/react";
 import Socials from "../src/components/Socials"
+import { toHaveAttribute } from "@testing-library/jest-dom";
 
 test("Socials renders without crashing", () => {
   render(<Socials />)
+})
+
+test("Meetup logo has link to Meetup page.", () => {
+  render(<Socials />)
+
+  expect(screen.getByRole('link', {name: "Meetup Logo /BendJS"})).toHaveAttribute(
+    "href",
+    "https://www.meetup.com/BendJS/"
+  );
+})
+
+test("Slack logo has link to Slack page.", () => {
+  render(<Socials />)
+
+  expect(screen.getByRole('link', {name: "Slack Logo"})).toHaveAttribute(
+    "href",
+    "https://bendjs.slack.com/ssb/redirect"
+  );
+})
+
+test("Youtube logo has link to Youtube page.", () => {
+  render(<Socials />)
+  
+  expect(screen.getByRole('link', {name: "Youtube Logo"})).toHaveAttribute(
+    "href",
+    "https://www.youtube.com/channel/UCCT744K1sGX-lPtaEivXCDQ"
+  );
 })
