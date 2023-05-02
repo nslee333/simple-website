298b7679686de0da4ae4d61f9ae8297bb130eda7
Nathan <nslee333@gmail.com>
2023-05-02 15:45:42 -0700

    Added href test to Share.

--- __tests__/Share.test.jsx
+++ __tests__/Share.test.jsx
@@ -1,6 +1,13 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { toHaveAttribute } from "@testing-library/jest-dom";
 import Share from "../src/components/Share";
 
 test("Share renders without crashing", () => {
   render(<Share />);
+});
+
+test("Contact Us button has a href value of #contact", () => {
+  render(<Share />);
+
+  expect(screen.getByRole("link")).toHaveAttribute("href", "#contact");
 });
