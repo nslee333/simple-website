7f9526e9e7f62ddc9c8e9efac2c5f031cf5b97d1
Nathan <nslee333@gmail.com>
2023-05-02 15:25:18 -0700

    Extracted Newsletter and Share card into separate files.

---
+++ src/components/Share.js
@@ -0,0 +1,40 @@
+export default function Share() {
+  return (
+    <div
+      className="m-5 
+                  h-auto 
+                  w-[42rem] 
+                  min-w-[18.85rem] 
+                  rounded-md 
+                  bg-white 
+                  p-5 
+                  sm:h-[25rem]"
+    >
+      <h3 className="text-3xl italic xs:mt-2">Have something to share?</h3>
+      <h4 className="text-2xl italic xs:mt-10">
+        We're always looking for speakers
+      </h4>
+      <div className="italic xs:mt-10">
+        If you have something to share, big or small, big or small, please
+        get in touch!
+      </div>
+      <div className="mb-2 mt-10 sm:mt-32">
+        <a
+          htmlFor="button"
+          href="#contact"
+          className="shadow-grey 
+                      rounded-lg 
+                      bg-[#7FE7D7] 
+                      p-3 
+                      text-2xl 
+                      shadow 
+                      hover:origin-bottom 
+                      hover:translate-y-0.5 
+                      hover:shadow-none"
+        >
+          Contact Us
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/components/StayConnected.js
+++ src/components/StayConnected.js
@@ -1,58 +1,12 @@
-import SignupForm from "./SignupForm";
+import Newsletter from "./Newsletter";
+import Share from "./Share";
 
 export default function Connected() {
   return (
     <>
       <div className="flex justify-evenly xs:flex-wrap">
-        <div className="m-5 h-auto w-[42rem] min-w-[18.85rem] rounded-md bg-white p-5 sm:h-[25rem]">
-          <div className="">
-            <h3 className="p-2 text-3xl italic">Newsletter</h3>
-            <h4 className="p-2 text-2xl italic">Stay up to date</h4>
-            <div className="p-2 italic">
-              Sign up for our monthly newsletter where we share local events in
-              Bend, tech news and our community member spotlight!
-            </div>
-          </div>
-          <div className="mx-2 my-2 flex">
-            <SignupForm />
-          </div>
-        </div>
-        <div
-          className="m-5 
-                      h-auto 
-                      w-[42rem] 
-                      min-w-[18.85rem] 
-                      rounded-md 
-                      bg-white 
-                      p-5 
-                      sm:h-[25rem]"
-        >
-          <h3 className="text-3xl italic xs:mt-2">Have something to share?</h3>
-          <h4 className="text-2xl italic xs:mt-10">
-            We're always looking for speakers
-          </h4>
-          <div className="italic xs:mt-10">
-            If you have something to share, big or small, big or small, please
-            get in touch!
-          </div>
-          <div className="mb-2 mt-10 sm:mt-32">
-            <a
-              htmlFor="button"
-              href="#contact"
-              className="shadow-grey 
-                          rounded-lg 
-                          bg-[#7FE7D7] 
-                          p-3 
-                          text-2xl 
-                          shadow 
-                          hover:origin-bottom 
-                          hover:translate-y-0.5 
-                          hover:shadow-none"
-            >
-              Contact Us
-            </a>
-          </div>
-        </div>
+        <Newsletter />
+        <Share />
       </div>
     </>
   );
