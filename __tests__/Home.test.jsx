e0c7799837e6f0bb3e9a2ffd385badc29236f4f0
Nathan <nslee333@gmail.com>
2023-05-02 20:54:55 -0700

    More snapshot tests.

--- __tests__/Home.test.jsx
+++ __tests__/Home.test.jsx
@@ -1,7 +1,16 @@
 import { render } from "@testing-library/react";
 import Home from "../src/pages/Home";
 import "@testing-library/jest-dom";
+import renderer from "react-test-renderer";
 
 test("Renders Home without crashing", () => {
   render(<Home />);
 });
+
+test("Home renders correctly according to snapshot", () => {
+  const tree = renderer
+    .create(<Home />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+})
--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -2,10 +2,18 @@ import { urlFor } from "../src/pages/index.js";
 import { render } from "@testing-library/react";
 import App from "../src/pages/index.js"
 import {client, getStaticProps, builder} from "../src/pages/index.js"
-
+import renderer from "react-test-renderer";
 
 test("App renders without crashing", () => {
   render(<App />)
+})
+
+test("App renders correctly according to snapshot", () => {
+  const tree = renderer
+    .create(<App />)
+    .toJSON();
+  
+  expect(tree).toMatchSnapshot();
 })
 
 test("client is defined", () => {
